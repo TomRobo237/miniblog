5056e4d3737a707e02d0b2258dbb43a95b745444
TomRobo237 <millertime1989@live.com>
2019-11-23 13:43:43 -0500

    Updating, at step before adding avatars

--- app/routes.py
+++ app/routes.py
@@ -48,6 +48,7 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -62,3 +63,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test POST #1'},
+        {'author': user, 'body': 'Test POST #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
