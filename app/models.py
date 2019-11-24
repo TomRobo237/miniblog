2a0c50ae96757b447232a484b622ce25c09b1bb6
TomRobo237 <millertime1989@live.com>
2019-11-24 17:12:33 -0500

    Chapter 6, Profile pages and editing profiles

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+from hashlib import md5
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -12,6 +13,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=dt.utcnow)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -21,6 +24,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+    # Potential TODO: Make an option to use a locally hosted file.
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('UTF-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
