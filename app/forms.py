59ad9a4a42b91939d324eda59fb4a5500b80fd44
TomRobo237 <millertime1989@live.com>
2019-11-30 19:49:20 -0500

    Bigfix and typo fix

--- app/forms.py
+++ app/forms.py
@@ -36,11 +36,11 @@ class EditProfileForm(FlaskForm):
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self.__init__(*args, **kwargs))
+        super(EditProfileForm, self).__init__(*args, **kwargs)
         self.original_username = original_username
 
     def validate_username(self, username):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
-            if User is not None:
+            if user is not None:
                 raise ValidationError('Username in use, please use a different username.')
